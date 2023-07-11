d31731efddda7aee92d520f9ddc35e5cba76851c
Simone Roveri <spikeroveri91@gmail.com>
2023-07-11 19:15:15 +0900

    To create a bookmark controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,4 +7,20 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
     @movies = @list.movies.all
   end
+
+  def new
+    @list = List.new
+  end
+
+  def create 
+    @list = List.new(list_params)
+    @list.save
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end

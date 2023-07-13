bcd852606b2740744a56a1e9211f09d02ae7d02f
Simone Roveri <spikeroveri91@gmail.com>
2023-07-13 22:09:00 +0900

    add Bookmark

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,17 +1,19 @@
 class BookmarksController < ApplicationController
-  before_action :set_list
+  before_action :set_bookmark, only: :destroy
+  before_action :set_list, only: [:new, :create]
   
   def new 
-    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
-    @bookmark.movie = @movie
     @bookmark.list = @list
-    @bookmark.save
-    redirect_to list_path(@list)
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
   end
 
   private
@@ -20,8 +22,13 @@ class BookmarksController < ApplicationController
     @list = List.find(params[:list_id])
   end
 
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+
   def bookmark_params
-    params.require(:bookmark).permit(:comment)
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: 'lists#index'
-  resources :lists do 
-    resources :bookmarks, only: [:new, :create, :destroy]
+  resources :lists, except: [:edit, :update] do 
+    resources :bookmarks, only: [:new, :create]
   end
+  resources :bookmarks, only: :destroy
 end
